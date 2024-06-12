bb826f48160d4db88a8fa27d452d49b73592d60e
FRANCISCOJESUS1980 <jesusgonzalezvergara@hotmail.com>
2024-06-12 22:31:13 +0200

    FEAT cambios principales

--- main.js
+++ main.js
@@ -1,24 +1,93 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import './src/styles/global/main.scss'
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
-
-setupCounter(document.querySelector('#counter'))
+const productos = [
+  {
+    name: 'bolero',
+    price: 450,
+    stars: 4,
+    kilos: 12,
+    seller: 'Superdomesticos Cabrera sl',
+    image:
+      'https://media.cecotec.cloud/02376/bolero-dresscode-121000-steel_4j2pdd_2.png:md'
+  },
+  {
+    name: 'infiniton',
+    price: 280,
+    stars: 3,
+    kilos: 6,
+    seller: 'Superdomesticos Cabrera sl',
+    image: 'https://infiniton.es/3984-large_default/wm-a62b.jpg'
+  },
+  {
+    name: 'chiq',
+    price: 250,
+    stars: 4,
+    kilos: 8,
+    seller: 'Superdomesticos Cabrera sl',
+    image:
+      'https://storage.googleapis.com/catalog-pictures-carrefour-es/catalog/pictures/hd_510x_/8592344703683_1.jpg'
+  },
+  {
+    name: 'siemens',
+    price: 350,
+    stars: 5,
+    kilos: 10,
+    seller: 'Superdomesticos Cabrera sl',
+    image:
+      'https://cdnebasnet.com/data/eshop/funnatic/images/products/i/1/3/6/9/0/p13690/0000-17905736-wg44g20xes-stp-def.webp?1667487120'
+  },
+  {
+    name: 'samsung',
+    price: 750,
+    stars: 3,
+    kilos: 12,
+    seller: 'Superdomesticos Cabrera sl',
+    image:
+      'https://i5.walmartimages.com.mx/mg/gm/1p/images/product-images/img_large/00880609001454l.jpg?odnHeight=612&odnWidth=612&odnBg=FFFFFF'
+  },
+  {
+    name: 'miele',
+    price: 650,
+    stars: 2,
+    kilos: 9,
+    seller: 'Superdomesticos Cabrera sl',
+    image:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQfqiKyA48YgbB5X5-aPgwdhCaNt5MMJ6vxFFExvvIoJS9HOikgzxpYj7qNq-yJUgWAuc&usqp=CAU'
+  },
+  {
+    name: 'balay',
+    price: 260,
+    stars: 3,
+    kilos: 7,
+    seller: 'Superdomesticos Cabrera sl',
+    image:
+      'https://media3.bsh-group.com/Product_Shots/1200x675/MCSA00390576_3TS853_def.jpg'
+  },
+  {
+    name: 'hisense',
+    price: 190,
+    stars: 4,
+    kilos: 5,
+    seller: 'Superdomesticos Cabrera sl',
+    image:
+      'https://www.hisense.es/wp-content/uploads/2022/03/Destacada-WFQY1014EVJM.png'
+  },
+  {
+    name: 'bosch',
+    price: 550,
+    stars: 5,
+    kilos: 10,
+    seller: 'Superdomesticos Cabrera sl',
+    image:
+      'https://cdn.grupoelcorteingles.es/SGFM/dctm/MEDIA03/202210/27/00104710390594____7__640x640.jpg'
+  },
+  {
+    name: 'aspes',
+    price: 150,
+    stars: 4,
+    kilos: 6,
+    seller: 'Superdomesticos Cabrera sl',
+    image:
+      'https://static.carrefour.es/hd_510x_/crs/cdn_static/catalog/hd/164390_00_1.jpg'
+  }
+]
